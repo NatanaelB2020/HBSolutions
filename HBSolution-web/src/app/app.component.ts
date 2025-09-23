0d30d64f427a8505e529e3b0c9b42f6ebd0f7153
Natanael Amaral de Barros <87382071+NatanaelB2020@users.noreply.github.com>
2025-09-23 14:17:44 -0300

    alteracao no front para ajustar erros

--- HBSolution-web/src/app/app.component.ts
+++ HBSolution-web/src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { LoginComponent } from './login/login.component';
+import { MenuComponent } from './menu/menu.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, LoginComponent], // Importa o LoginComponent
-  template: `<app-login></app-login>`, // Mostra o login direto
+  imports: [CommonModule, MenuComponent], // Adicionando o componente em imports
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {}
--- HBSolution-web/src/app/app.routes.ts
+++ HBSolution-web/src/app/app.routes.ts
@@ -1,14 +1,2 @@
-// src/app/app.routes.ts
-import { Routes, provideRouter } from '@angular/router';
-import { LoginComponent } from './login/login.component';
-
-export const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: 'login', component: LoginComponent }
-];
-
-export const appConfig = {
-  providers: [
-    provideRouter(routes)
-  ]
-};
+import { Routes } from '@angular/router';
+export const routes: Routes = [];
